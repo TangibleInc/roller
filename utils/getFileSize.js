2c3a0cf411d837f8fb77ebb4dfcee7dde15e1af2
eliot-akira <me@eliotakira.com>
2021-06-26 21:23:57 +0200

    Improve fize size method when file does not exist

--- utils/getFileSize.js
+++ utils/getFileSize.js
@@ -2,10 +2,18 @@
 const fs = require('fs')
 
 async function getFileSize(filename) {
-  const stats = await fs.promises.stat(filename)
-  const { size } = stats
-  const i = Math.floor(Math.log(size) / Math.log(1024))
-  return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i]
+
+  try {
+    const stats = await fs.promises.stat(filename)
+    const { size } = stats
+
+    const i = Math.floor(Math.log(size) / Math.log(1024))
+    return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i]
+
+  } catch(e) {
+    return 'NOT FOUND'
+  }
+
 }
 
 module.exports = getFileSize
