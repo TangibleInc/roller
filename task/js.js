cc4a6fee044a268c4b030cd1239476c5cb482259
Nicolas Jaussaud <nicolas@teamtangible.com>
2024-04-07 05:14:06 -0400

    JS task: Compatibility between importToGlobal and raw

--- task/js.js
+++ task/js.js
@@ -298,6 +298,8 @@ function createOptionsForTaskType(config, task) {
         ...(task.esbuild || {}),
       }),
 
+      ...(task.raw ? [raw(task.raw)] : []),
+
       /**
        * Transform imports into global variables
        *
@@ -382,8 +384,6 @@ function createOptionsForTaskType(config, task) {
        * @see https://github.com/rollup/plugins/tree/master/packages/inject
        */
       inject(globalToImport),
-
-      ...(task.raw ? [raw(task.raw)] : []),
 
       // Custom Rollup plugins per task
       ...(task.rollupPlugins || []),
